6b4a943064dd52ae0bb7491e7164d11813f048e7
Caesar <sirostaib@gmail.com>
2022-01-31 04:51:26 +0300

    update 1.0
    register is now working.

--- wt/register.php
+++ wt/register.php
@@ -1,19 +1,43 @@
 
 <?php 
 
-if(isset($_POST['submit'])){
 
-    $username = $_POST["username"];
-    $email = $_POST['password'];
-    $phone = $_POST['phone'];
-    $password = md5($_POST['password']);
 
-    $sql = "insert tno new_table($username,$email,$password)
-            values('$usernsmr','$email','$password',)";
-
-    $result = mysql_query($conn,$sql);
+ 
 
       
+
+
+
+  
+if( ( !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['fullname']) && !empty($_POST['username']) ) ) {  
+    $email=$_POST['email'];  
+    $passwordd=$_POST['password'];  
+    $fullname=$_POST['fullname'];  
+    $usernamee=$_POST['username']; 
+  
+    $servername='mysql-67581-0.cloudclusters.net:19366';
+    $username='admin';
+    $password='a3JILML2';
+    $dbname = "web";
+    $con=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$con){
+          die('Could not Connect MySql Server:' .mysql_error());
+        }
+  
+
+        $sql = "INSERT INTO account (fullname, password, email, username)
+VALUES ('$fullname', '$passwordd', '$email', '$usernamee')";
+
+if ($con->query($sql) === TRUE) {
+  echo "New account record created successfully";
+  header("Location: login.html");  
+} else {
+  echo "Error: " . $sql . "<br>" . $con->error;
+  header("Location: register.html");  
+}
+
+$con->close();
 
 }
 
